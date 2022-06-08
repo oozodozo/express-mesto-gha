17401915e97aadb39deab2ad8d5a3ffb549d80ed
Ivan Dozorov <oozo1101@yandex.ru>
2022-06-09 01:29:28 +0300

    feat: add user _id

--- app.js
+++ app.js
@@ -9,6 +9,12 @@ const app = express();
 app.use(express.json());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use((req, res, next) => {
+  req.user = {
+    _id: '62a1210bb5bd5e50da558fbe',
+  };
+  next();
+});
 app.use(userRoutes);
 
 mongoose.connect('mongodb://localhost:27017/mestodb');
@@ -16,5 +22,3 @@ mongoose.connect('mongodb://localhost:27017/mestodb');
 app.listen(PORT, () => {
   console.log(`App started on ${PORT} port`);
 });
-
-
