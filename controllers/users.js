03f43744aa3e34260a11ef47ecd37ac96f140f16
Ivan Dozorov <oozo1101@yandex.ru>
2022-06-08 23:52:19 +0300

    feat: add controllers users

---
+++ controllers/users.js
@@ -0,0 +1,23 @@
+const User = require('../models/user');
+
+const getUsers = (req, res) => {
+  User.find({})
+    .then((users) => res.send({ data: users }))
+    .catch(() => res.status(500).send({ message: 'Произошла ошибка' }));
+};
+
+const getUserById = (req, res) => {
+  User.findById(req.params._id)
+    .then((user) => res.send({ data: user }))
+    .catch(() => res.status(500).send({ message: 'Произошла ошибка' }));
+};
+
+const createUser = (req, res) => {
+  const { name, about, avatar } = req.body;
+
+  User.create({ name, about, avatar })
+    .then((user) => res.send({ data: user }))
+    .catch(() => res.status(500).send({ message: 'Произошла ошибка' }));
+};
+
+module.exports = { getUsers, getUserById, createUser };
