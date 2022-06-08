71a9ea6c3d0c08d6a3ae4b3cf44a32230d55dc5b
Ivan Dozorov <oozo1101@yandex.ru>
2022-06-09 01:23:45 +0300

    feat: add routes users

--- app.js
+++ app.js
@@ -1,16 +1,20 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
+const userRoutes = require('./routes/users');
 
 const { PORT = 3000 } = process.env;
 const app = express();
 
-mongoose.connect('mongodb://localhost:27017/mestodb', {
-  useNewUrlParser: true,
-  useCreateIndex: true,
-  useFindAndModify: false,
+app.use(express.json());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(userRoutes);
+
+mongoose.connect('mongodb://localhost:27017/mestodb');
+
+app.listen(PORT, () => {
+  console.log(`App started on ${PORT} port`);
 });
 
-app.use(bodyParser.json());
 
-app.listen(PORT);
--- controllers/users.js
+++ controllers/users.js
@@ -3,13 +3,13 @@ const User = require('../models/user');
 const getUsers = (req, res) => {
   User.find({})
     .then((users) => res.send({ data: users }))
-    .catch(() => res.status(500).send({ message: 'Произошла ошибка' }));
+    .catch((err) => res.status(500).send({ message: err }));
 };
 
 const getUserById = (req, res) => {
   User.findById(req.params._id)
     .then((user) => res.send({ data: user }))
-    .catch(() => res.status(500).send({ message: 'Произошла ошибка' }));
+    .catch((err) => res.status(500).send({ message: err }));
 };
 
 const createUser = (req, res) => {
@@ -17,7 +17,7 @@ const createUser = (req, res) => {
 
   User.create({ name, about, avatar })
     .then((user) => res.send({ data: user }))
-    .catch(() => res.status(500).send({ message: 'Произошла ошибка' }));
+    .catch((err) => res.status(500).send({ message: err }));
 };
 
 module.exports = { getUsers, getUserById, createUser };
