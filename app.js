08cd2522d26c5fa7437e11141dcb7cf1191749fd
Ivan Dozorov <oozo1101@yandex.ru>
2022-06-08 22:39:43 +0300

    feat: add mongodb

--- app.js
+++ app.js
@@ -1,8 +1,16 @@
-const express = require('express')
+const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
 
-const { PORT = 3000 } = process.env
-const app = express()
+const { PORT = 3000 } = process.env;
+const app = express();
 
-app.listen(PORT, () => {
-  console.log(`Сервер удачно запустился и мы слушаем ${PORT} порт`);
-})
+mongoose.connect('mongodb://localhost:27017/mestodb', {
+  useNewUrlParser: true,
+  useCreateIndex: true,
+  useFindAndModify: false,
+});
+
+app.use(bodyParser.json());
+
+app.listen(PORT);
