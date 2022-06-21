d155a7f271208651970c4de5678d172a13b4b448
Ivan Dozorov <oozo1101@yandex.ru>
2022-06-22 00:02:41 +0300

    fix: error test 14

--- models/user.js
+++ models/user.js
@@ -18,11 +18,11 @@ const userSchema = new mongoose.Schema({
   },
   avatar: {
     type: String,
+    default: 'https://pictures.s3.yandex.net/resources/jacques-cousteau_1604399756.png',
     validate: {
       validator: (link) => validator.isURL(link),
       message: 'Неправильный формат ссылки',
     },
-    default: 'https://pictures.s3.yandex.net/resources/jacques-cousteau_1604399756.png',
   },
   email: {
     type: String,
